22745dca1e64d8add56f9a0a27f613dc96da0586
Marcus Wu <mwu@digitaltorque.com>
2026-01-08 09:27:45 -0500

    Fix vertical and horizontal constraints

--- ParallelConstraint.go
+++ ParallelConstraint.go
@@ -22,9 +22,6 @@ func (s *Sketch) AddHorizontalConstraint(p1 *Element) (*Constraint, error) {
 	if p1.elementType != Line && p1.elementType != Axis {
 		return nil, errors.New("incorrect element types for vertical constraint")
 	}
-	if p1.element.AsLine().Start.X.Cmp(&p1.element.AsLine().End.X) > 0 {
-		return s.AddAngleConstraint(p1, s.XAxis, 180, false)
-	}
 	return s.AddParallelConstraint(p1, s.XAxis)
 }
 
@@ -32,9 +29,6 @@ func (s *Sketch) AddVerticalConstraint(p1 *Element) (*Constraint, error) {
 	// Check p1's points to see if start is below end; if so, angle shold be 180 degrees
 	if p1.elementType != Line && p1.elementType != Axis {
 		return nil, errors.New("incorrect element types for vertical constraint")
-	}
-	if p1.element.AsLine().Start.Y.Cmp(&p1.element.AsLine().End.Y) > 0 {
-		return s.AddAngleConstraint(p1, s.YAxis, 180, false)
 	}
 	return s.AddParallelConstraint(p1, s.YAxis)
 }
